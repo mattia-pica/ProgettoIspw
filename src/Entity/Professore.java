791a68e135e267ed4b4f1427a64b0b5beee1e258
mattiapica94 <mattia_94p@hotmail.it>
2018-02-02 10:39:45 +0100

    CLASSE DI PROVA

---
+++ src/Entity/Professore.java
@@ -0,0 +1,31 @@
+package Entity;
+
+public class Professore {
+
+    public String username;
+    public String password;
+
+    public Professore(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
